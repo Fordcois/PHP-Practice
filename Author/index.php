9cb6a8421536b3a649f307518cbf8777e8904404
Sam Ford <samueljamesford@googlemail.com>
2024-03-14 10:56:48 +0000

    establishes connection to mysql database

--- Author/index.php
+++ Author/index.php
@@ -1,8 +1,20 @@
 <?php
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+// connect to the MYSQL database
+$dsn ='mysql:host=localhost;port=3306;dbname=author;user=root;charset=utf8mb4';
+
+$pdo = new PDO($dsn);
+
+$statement= $pdo->prepare('select * from blog');
+
+$statement ->execute();
+
+$posts = $statement ->fetchAll();
 
 
 
 
+dd($posts);
 
