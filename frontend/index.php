086798e9c8e7e458c584f07cb9ca84b114e6647d
Sam Ford <samueljamesford@googlemail.com>
2024-04-11 18:30:24 +0100

    renames frontend file & updates README

--- frontend/index.php
+++ frontend/index.php
@@ -1,15 +1,14 @@
 <?php
 
 require 'functions.php';
-//require 'router.php';
+require 'router.php';
 require 'Database.php';
 
 $config = require('config.php');
 $db = new Database($config['database']);
 
 $id = $_GET['id'];
-$query = "select * from blog where id = :id";
+$query = "select * from blog where id = 1";
 
 $posts = $db->query($query, [':id' => $id])->fetch();
 
-dd($posts);
